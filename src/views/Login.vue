64c7fc0a38b1fad7ac26528e10ae303864175317
João Pedro <joao@agys.com.br>
2020-10-17 05:07:22 -0300

    Firebase e fiery instalado

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    v-text-field(
+        v-model="login.email",
+        label="E-mail",
+        type="email",
+        required,
+        dark,
+        hide-details,
+        style="margin-bottom: 15px"
+        )
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+
