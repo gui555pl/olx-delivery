ec81ddffcde75c0d3351261afd8f5909681bf627
gui55555pl <guilherme@beyondcloud.com.br>
2020-10-17 05:49:46 -0300

    feat: baseLayout done

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -2,10 +2,10 @@
   span
     v-app-bar(app color="#6E0AD6" prominent flat shrink-on-scroll dark)
       v-btn(icon style='position: absolute; left: 0; top: 0; margin: 4px 0 0 4px;')
-        v-icon mdi-arrow-left
-      div(style='align-self: center !important; margin: auto; font-size: 80%; font-weight: 600; padding: 8px 0 0 0; text-align: center;') Por favor, confirme os dados seguintes para finalizar sua compra 
-      //- v-row a
-      //- div(style='left: 0; bottom: 20px; padding: 0 16px;') Por favor seu lixo Por favor seu lixoa Por favor seu lixo Por favor seu lixoa Por favor seu lixo Por favor seu lixoa 
+        v-icon(@click='$router.go(-1)') mdi-arrow-left
+      div(:style='{ color: "rgba(255, 255, 255," + textOpacity + ")" }' style='align-self: center !important; margin: auto; font-size: 80%; font-weight: 600; padding: 8px 0 0 0; text-align: center;') {{ text }} 
+      .avatar-custom
+        v-icon {{ icon }}
     v-main
       zoom-center-transition(:duration="200" mode="out-in")
         router-view
@@ -14,25 +14,35 @@
 <script>
 export default {
   name: 'BaseLayout',
+  props: {
+    icon: {
+      type: String,
+      default: "mdi-file-document-outline"
+    },
+    text: {
+      type: String,
+      default: "Por favor, confirme os dados seguintes para finalizar sua compra"
+    }
+  },
   computed: {
     headerHeight () {
       return this.$el.querySelector('header').style.height
-    },
-    textOpacity () {
-      let op
-      const F = this.heightPercentage
-      op = (result - 43.75 / 100 - 43.75) = (F/ 100) 
     }
   },
   data () {
     return {
-      heightPercentage: 100
+      heightPercentage: 100,
+      textOpacity: 100,
     }
   },
   methods: {
     updatedToolbarHeight (event) {
       const height = parseInt(this.$el.querySelector('header').style.height)
       this.heightPercentage = (height/128)*100
+      let op
+      const F = this.heightPercentage
+      this.textOpacity = (1.77777778 * F - 77.77777778)/100
+      
     }
   },
   created () {
@@ -47,5 +57,21 @@ export default {
 <style>
 .v-toolbar__content {
   flex-direction: column !important;
+}
+.avatar-custom {
+  position: absolute;
+  bottom: -24px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  vertical-align: middle;
+  border-radius: 50%;
+  border: 2px solid white;
+  height: 48px;
+  width: 48px;
+  background: orange;
 }
 </style>
