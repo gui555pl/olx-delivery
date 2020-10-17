282c64afee4b463fd26dfaf5748daa431f073cb0
João Pedro <joao@agys.com.br>
2020-10-17 04:52:00 -0300

    Firebase e fiery instalado

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
+import FieryVue from "fiery-vue";
+Vue.use(FieryVue);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  fiery: true,
+  render: (h) => h(App),
   vuetify,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBZ1UwuPy3SBIORYrPlYevP6QVwrCSA4TI",
+      authDomain: "olx-delivery.firebaseapp.com",
+      databaseURL: "https://olx-delivery.firebaseio.com",
+      projectId: "olx-delivery",
+      storageBucket: "olx-delivery.appspot.com",
+      messagingSenderId: "345906331366",
+      appId: "1:345906331366:web:272d67224c41e7892c8527",
+    });
+  },
+}).$mount("#app");
