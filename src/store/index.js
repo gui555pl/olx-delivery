1979447e441f07c8f4a4de2b5ac920cd5b6519d8
João Pedro <joao@agys.com.br>
2020-10-17 04:57:36 -0300

    Firebase e fiery instalado

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import VuexPersistence from 'vuex-persist'
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 Vue.use(Vuex)
 
 export default new Vuex.Store({
