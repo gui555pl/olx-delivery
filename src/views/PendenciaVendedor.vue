7ae2019ef81639d981132f6121bf9298df05c7d8
gui555pl <gpl2@cin.ufpe.br>
2020-10-22 20:19:49 -0300

    fix: product card center / pendencias

--- src/views/PendenciaVendedor.vue
+++ src/views/PendenciaVendedor.vue
@@ -1,13 +1,16 @@
 <template lang='pug'>
     div
-      v-col(style="text-align:center;" )
+      v-col
+        template(v-if='!loading')  
           v-row(style="margin-top:10%; justify-content:center;" v-if="produtos.length == 0")
               img(src="../imagens/sad.jpg" height="200px;" )
               h3(style="font-size:25px; color:#565656#; margin-top:20px; width:70%; ") Você não tem nenhuma venda pendente
-          v-row#product-list( v-else)
+          v-row#product-list(v-else)
               v-col.py-1(cols='12' v-for='(produto, i) in produtos' :key='i')
                   ProductCard(:product='produto' :isPendencia="true")
-
+        template(v-else)
+          v-col.py-1(cols='12' v-for='i in 6' :key='i')  
+            v-skeleton-loader(height='93.75px' type="list-item-avatar-three-line")
 </template>
 
 <script>
@@ -22,11 +25,17 @@ export default {
   },
   data() {
       return{
-        produtos: []
+        produtos: null,
+        loading: true
       }
   },
   mounted () {
-    this.produtos = this.$fiery(firebase.firestore().collection("produtos").where('status','==','aguardando_vendedor').where('vendedor_email', '==', this.$store.getters.user.email))
+    this.$fiery(firebase.firestore().collection("produtos").where('status','==','aguardando_vendedor').where('vendedor_email', '==', this.$store.getters.user.email), {
+      onSuccess: (data) => {
+        this.produtos = data
+        this.loading = false
+      }
+    })
   }
 };
 </script>
