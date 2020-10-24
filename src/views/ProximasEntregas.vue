208c796b7668331570f2bb64e77bcbf86ae0f66a
gui555pl <gpl2@cin.ufpe.br>
2020-10-24 07:11:00 -0300

    fix: added prazo based on frete

--- src/views/ProximasEntregas.vue
+++ src/views/ProximasEntregas.vue
@@ -1,7 +1,11 @@
 <template lang='pug'>
     div
+      v-col.pb-0.mt-5(cols='12' align-self='center' style='text-align: center;')
+        div.caption(style='display: inline-block;') Essas são as entregas mais próximas de você!&nbsp;
+          span(style='color: #FFA500') O tempo em laranja &nbsp;
+          | é quanto, em minutos, vai acrescentar na sua rota atual.
       v-col(style="text-align:center; " )
-        h3(style="margin-top:20px;") Entregas pendentes 
+        h3 Entregas pendentes 
         v-list.overflow-y-auto(style="max-height: 230px;")
           v-col.py-1(cols='12' v-for='(produto, i) in produtosNovos' :key='i')
             v-card(@click="toInfo(produto)")
@@ -9,11 +13,11 @@
                 v-col(cols='auto')
                   v-avatar(color='#00800030' size='48')
                     v-icon(color='#008000') mdi-cash-multiple
-                  p(style="display: inline; font-weight: 400px; color:green; margin-top:20px;")  R${{Math.floor(produto.frete*0.8)}},15
+                  p(style="display: inline; font-weight: 400px; color:green; margin-top:20px;")  R${{parseFloat((produto.frete*0.8).toFixed(2))}}
                 v-col(cols='auto')
                   v-avatar(color='#ffa50030' size='48')
                     v-icon(color='orange') mdi-timer-sand
-                  p(style="display: inline; font-weight: 400px; color:orange;margin-top:20px;")  {{getRandomInt(5,30)}} min
+                  p(style="display: inline; font-weight: 400px; color:orange;margin-top:20px;")  {{simulatePrazo(produto.frete)}} min
                     
 
               
@@ -57,8 +61,11 @@ export default {
     },
     getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    simulatePrazo (frete) {
+      const a = 5, b = -45
+      return Math.floor(a*frete + b)
     }
-
   },
   created(){
 
