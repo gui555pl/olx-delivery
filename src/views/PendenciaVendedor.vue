afc395b61b4132bc5ca52a0e5b5ef7eb53c62642
gui555pl <gpl2@cin.ufpe.br>
2020-10-24 08:48:03 -0300

    fix: pendenciaVendedor cols 12

--- src/views/PendenciaVendedor.vue
+++ src/views/PendenciaVendedor.vue
@@ -3,8 +3,10 @@
       v-col
         template(v-if='!loading')  
           v-row(style="margin-top:10%; justify-content:center; text-align: center;" v-if="produtos.length == 0")
+            v-col.pa-0(cols='12')
               img(src="../imagens/sad_test.png" height="200px;" )
-              h3(style="font-size:25px; color:#565656#; margin-top:20px; width:70%; ") Você não tem nenhuma venda pendente
+            v-col.pa-0.text-center(cols='12')  
+              h3(style="font-size:25px; color:#565656#; margin-top:20px; width:100%; ") Você não tem nenhuma venda pendente
           v-row#product-list(v-else)
               v-col.py-1(cols='12' v-for='(produto, i) in produtos' :key='i')
                   ProductCard(:product='produto' :isPendencia="true")
